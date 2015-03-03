54d8f6244e6f4489380a222b6b396d0ee01210be
Guillermo <guillermo.riverola@outlook.com>
2015-03-03 12:47:23 +0100

    Guillermo

---
+++ src/main/java/edu/upc/eetac/dsa/griverola/beeter/api/model/StingCollection.java
@@ -0,0 +1,26 @@
+package edu.upc.eetac.dsa.griverola.beeter.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class StingCollection {
+	private List<Sting> stings;
+ 
+	public StingCollection() {
+		super();
+		stings = new ArrayList<>();
+	}
+ 
+	public List<Sting> getStings() {
+		return stings;
+	}
+ 
+	public void setStings(List<Sting> stings) {
+		this.stings = stings;
+	} 
+	
+	public void addSting(Sting sting) {
+		stings.add(sting);
+		
+	}
+}
